596244141a70981c92d6412b18fa47ec2a676cd7
Wes Guirra <wesley.guirra@platformbuilders.io>
2019-10-25 17:35:21 -0300

    Implemented startPaymentV2 method

--- android/src/main/java/br/com/enjoyin/paystoreclient/flutter_paystore_client/FlutterPaystoreClientPlugin.java
+++ android/src/main/java/br/com/enjoyin/paystoreclient/flutter_paystore_client/FlutterPaystoreClientPlugin.java
@@ -1,10 +1,19 @@
 package br.com.enjoyin.paystoreclient.flutter_paystore_client;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import br.com.phoebus.android.payments.api.ApplicationInfo;
+import br.com.phoebus.android.payments.api.Credentials;
 import br.com.phoebus.android.payments.api.ErrorData;
 import br.com.phoebus.android.payments.api.PaymentClient;
 import br.com.phoebus.android.payments.api.PaymentRequestV2;
+import br.com.phoebus.android.payments.api.PaymentType;
 import br.com.phoebus.android.payments.api.PaymentV2;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -21,11 +30,36 @@ public class FlutterPaystoreClientPlugin implements MethodCallHandler {
   }
 
   @Override
-  public void onMethodCall(MethodCall call, final @NonNull Result result) {
+  public void onMethodCall(@NonNull MethodCall call, final @NonNull Result result) {
     PaymentClient paymentClient = new PaymentClient();
 
     if (call.method.equals("startPaymentV2")) {
       PaymentRequestV2 paymentRequest = new PaymentRequestV2();
+
+      @NonNull
+      String applicationId = Objects.requireNonNull(call.argument("applicationId"));
+      @NonNull
+      String token = Objects.requireNonNull(call.argument("token"));
+      @NonNull
+      int value = Integer.parseInt(Objects.requireNonNull(call.argument("value")));
+      @NonNull
+      String appVersion = Objects.requireNonNull(call.argument("appVersion"));
+      @NonNull
+      String transactionId = Objects.requireNonNull(call.argument("transactionId"));
+      @Nullable
+      List paymentTypes = new ArrayList(Objects.requireNonNull(call.argument("paymentTypes")));
+
+      ApplicationInfo appInfo = new ApplicationInfo();
+      Credentials credentials = new Credentials();
+      credentials.setApplicationId(applicationId);
+      credentials.setSecretToken(token);
+      appInfo.setCredentials(credentials);
+      appInfo.setSoftwareVersion(appVersion);
+
+      paymentRequest.setValue(new BigDecimal(value));
+      paymentRequest.setAppTransactionId(transactionId);
+      paymentRequest.setAppInfo(appInfo);
+      paymentRequest.setPaymentTypes(paymentTypes);
       PaymentClient.PaymentCallback<PaymentV2> paymentCallback = new PaymentClient.PaymentCallback<PaymentV2>() {
         @Override
         public void onSuccess(PaymentV2 paymentV2) {
--- lib/flutter_paystore_client.dart
+++ lib/flutter_paystore_client.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
 
 class FlutterPaystoreClient {
   static const MethodChannel _channel =
       const MethodChannel('flutter_paystore_client');
 
-  static Future<String> get startPaymentV2 async {
-    final String payment = await _channel.invokeMethod('startPaymentV2');
+  static Future<String> startPaymentV2({@required String transactionId, @required int value, @required String appVersion}) async {
+    final String payment = await _channel.invokeMethod('startPaymentV2', <String, dynamic>{'transactionId': transactionId, 'value': value, 'appVersion': appVersion});
     return payment;
   }
 }
