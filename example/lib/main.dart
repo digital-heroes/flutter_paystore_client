c240deb560f5907865c8461d47fa09b8330ec69b
Wes Guirra <wesley.guirra@platformbuilders.io>
2019-10-28 17:56:32 -0300

    Add more arguments to payment method

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      platformVersion = await FlutterPaystoreClient.startPaymentV2;
+      platformVersion = await FlutterPaystoreClient.startPaymentV2();
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
--- lib/flutter_paystore_client.dart
+++ lib/flutter_paystore_client.dart
@@ -7,8 +7,22 @@ class FlutterPaystoreClient {
   static const MethodChannel _channel =
       const MethodChannel('flutter_paystore_client');
 
-  static Future<String> startPaymentV2({@required String transactionId, @required int value, @required String appVersion}) async {
-    final String payment = await _channel.invokeMethod('startPaymentV2', <String, dynamic>{'transactionId': transactionId, 'value': value, 'appVersion': appVersion});
+  static Future<String> startPaymentV2({
+    @required String transactionId,
+    @required int value,
+    @required String appVersion,
+    @required String applicationId,
+    @required String token,
+    @required List paymentTypes
+  }) async {
+    final String payment = await _channel.invokeMethod('startPaymentV2', <String, dynamic>{
+      'transactionId': transactionId,
+      'value': value,
+      'appVersion': appVersion,
+      'token': token,
+      'applicationId': applicationId,
+      'paymentTypes': paymentTypes,
+    });
     return payment;
   }
 }
